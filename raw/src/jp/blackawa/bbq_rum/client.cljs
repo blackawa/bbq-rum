361db818e87d3f480100c8625da597b721b73a2c
blackawa <3104kurokawa@gmail.com>
2018-03-26 21:40:26 +0900

    :tada: shopping cart

--- raw/src/jp/blackawa/bbq_rum/client.cljs
+++ raw/src/jp/blackawa/bbq_rum/client.cljs
@@ -12,13 +12,20 @@
 (rum/defc +counter < rum/reactive [state]
   [:.card
    [:.card-content
-    [:h2.title.is-4 "Current count:" (:count (rum/react state))]
-    [:button.button.is-success {:on-click #(swap! state update-in [:count] inc)} "Increment++"]]])
+    [:.navbar.is-light
+     [:.navbar-brand
+      [:a.navbar-item {:href "#"} "RumShop"]]
+     [:.navbar-end
+      [:a.navbar-item {:href "#"}
+       [:span.icon [:i.fas.fa-shopping-cart]]
+       [:span "Cart(" (:count (rum/react state)) ")"]]]]
+    [:h2.title.is-4 "Lamb Chop 1kg"]
+    [:button.button.is-success {:on-click #(swap! state update-in [:count] inc)} "Add to cart"]]])
 
 (rum/defc +app [state]
   [:.content
    [:h1.title "Welcome to Rum world!"]
-   (+counter state)])
+   [:columns (+counter state)]])
 
 (when-let [app (js/document.getElementById "app")]
   (rum/mount (+app state) app))
